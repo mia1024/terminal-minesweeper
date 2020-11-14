2d41ee3f052c19b983a19075045b57121549ba6f
Mia Celeste <mia@miaceleste.dev>
2020-11-13 20:09:56 -0800

    Added more system checks

--- __main__.py
+++ __main__.py
@@ -2,17 +2,17 @@ import sys, os, time
 from config import config
 
 
-def print_slow(text: str, trailing_newline=True):
+def print_slow(text: str, trailing_newline=True, delay=0.035):
     for i in range(len(text) + 1):
         print('\r' + text[:i], end='', flush=True)
-        time.sleep(0.035)
+        time.sleep(delay)
     if trailing_newline: print()
 
 
-def check(condition: bool, name: str, err_message: str):
-    if config.silent_check:
-        if not condition:
-            print('Error: '+err_message)
+def check(condition: bool, name: str, error_message: str):
+    if config.silent_checks:
+        if not (condition or config.ignore_failures):
+            print('Error: ' + error_message)
             sys.exit(1)
         else:
             return
@@ -23,36 +23,61 @@ def check(condition: bool, name: str, err_message: str):
             print(f"\r{prefix}\033[92m{'PASSED'[:i]}\033[0m", flush=True, end='')
         print()
     else:
-        for i in range(7):
-            print(f"\r{prefix}\033[91m{'FAILED'[:i]}\033[0m", flush=True, end='')
-        print('\033[91m')
-        print_slow(err_message)
-        print('\033[0m')
-        sys.exit(1)
+        if not config.ignore_failures:
+            for i in range(7):
+                print(f"\r{prefix}\033[91m{'FAILED'[:i]}\033[0m", flush=True, end='')
+            print('\033[91m')
+            print_slow(error_message)
+            print('\033[37m', end='')
+            print_slow('Alternatively, you may pass in the --ignore-failure switch.', delay=0.01)
+            print_slow('However, you will probably encounter a curses error if you do so.', delay=0.01)
+            print('\033[0m', end='')
+            sys.exit(1)
+        else:
+            for i in range(16):
+                print(f"\r{prefix}\033[91m{'FAILURE IGNORED'[:i]}\033[0m", flush=True, end='')
+            print()
 
-if not config.silent_check:
+
+if not config.silent_checks:
     print_slow('Welcome to terminal minesweeper by Mia Celeste')
-    print_slow('Running compatibility check...')
+    print_slow('Commencing system check...')
 
 v = sys.version_info
 check(v.major == 3 and v.minor >= 8,
       'Python version',
-      'Please use Python3.8 or above'
+      'Please use Python3.8 or above.'
       )
 
 check('xterm' in os.environ.get('TERM', ''),
       'terminal compatibility',
       'Please use an xterm-compatible terminal'
+      ' such as MinTTY' if sys.platform == 'win32' else '.'
+      )
+
+try:
+    import curses
+except:
+    curses_ok = False
+else:
+    curses_ok = True
+
+check(curses_ok, 'if curses is present',
+      'Cannot import curses, please check your Python installation.'
+      ' Perhaps try installing Cygwin or windows-curses?' if sys.platform == 'win32' else ''
       )
 
 width, height = os.get_terminal_size()
 
-min_width = config.board_width * 5 + 10
+min_width = config.board_width * 5 + 1 + 10 + 22
 min_height = config.board_height * 2 + 4
-check(width >= min_width, 'terminal width',
-      f'Please make sure your terminal has at least {min_width} columns')
-check(height >= min_height, 'terminal height',
-      f'Please make sure your terminal has at least {min_height} rows')
+check(width >= min_width, 'window width',
+      f'Please make sure your terminal window has at least {min_width} columns.')
+check(height >= min_height, 'window height',
+      f'Please make sure your terminal window has at least {min_height} rows.')
+
+if not config.silent_checks:
+    print_slow('All system checks completed （＾ω＾） ready to sweep some mines ┗(＾0＾)┓')
 
 from ui import main
 
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import argparse,sys
+
 class SingletonMeta(type):
     def __call__(cls, *args, **kwargs):
         if hasattr(cls, '_instance'):
@@ -15,8 +17,47 @@ class Config(metaclass=SingletonMeta):
         self.framerate = 60
         self.board_width = 16
         self.board_height = 16
-        self.mine_count = 10
-        self.silent_check = True
+        self.mine_count = 40
+        self.silent_checks = False
+        self.ignore_failures = False
 
 
 config = Config()
+parser = argparse.ArgumentParser(prog='terminal-minesweeper', add_help=False)
+g=parser.add_argument_group('Options')
+group = g.add_mutually_exclusive_group()
+group.add_argument('-e', '--easy', action='store_true',
+                   help='Set the game difficulty to easy (9x9 board with 10 mines)')
+group.add_argument('-i', '--intermediate', action='store_true',
+                   help='Set the game difficulty to intermediate (16x16 board with 40 mines). This is the default.')
+group.add_argument('-h', '--hard', action='store_true',
+                   help='Set the game difficulty to hard (16x30 board with 99 mines).')
+group.add_argument('-c', '--custom', nargs=3, type=int, metavar=('width', 'height', 'mines'),
+                   help='Set a custom game difficulty.')
+
+g.add_argument('--silent-checks', action='store_true',
+                    help='Performs the initial system checks quickly and quietly. '
+                         'This option can significantly increase the game startup speed.')
+g.add_argument('--ignore-failures', action='store_true',
+                    help='Ignore all failures in the initial system checks. Not recommended.')
+g.add_argument('--help', action='store_true', help='Show this help message and exit.')
+args = parser.parse_args()
+
+if args.help:
+    parser.print_help()
+    sys.exit(0)
+
+config.ignore_failures=args.ignore_failures
+config.silent_checks=args.silent_checks
+
+if args.easy:
+    config.board_width=9
+    config.board_height=9
+    config.mine_count=10
+elif args.hard:
+    config.board_width=16
+    config.board_height=30
+    config.mine_count=99
+elif args.custom:
+    config.board_width,config.board_height,config.mine_count=args.custom
+
