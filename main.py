2a87b97831c4888acf9a9edc1263a4014c031307
Mia Celeste <mia@miaceleste.dev>
2020-11-16 13:34:00 -0800

    Animations on entering/leaving the main window

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import signal
 
 
 def sigint_handler(signum, frame):
-    print('\033[0m\nAlright. Alright. You aren\'t sweeping any mines today')
+    print('\033[0m\nAlright. Alright. We aren\'t sweeping any mine today (T＿T)')
     print('\033[?25h', end='')  # reenable cursor
     sys.exit(2)
 
@@ -13,10 +13,12 @@ system_sigint_handler = signal.getsignal(signal.SIGINT)
 signal.signal(signal.SIGINT, sigint_handler)
 
 
-def print_slow(text: str, trailing_newline=True, prefix='', suffix='', delay=0.02):
-    print(prefix, flush=True, end='')
+def print_slow(text: str, trailing_newline=True, prefix='', suffix='', delay=0.015):
+    sys.stdout.write(prefix)
+    sys.stdout.flush()
     for char in text:
-        print(char, end='', flush=True)
+        sys.stdout.write(char)
+        sys.stdout.flush()
         time.sleep(delay)
     print(suffix, flush=True, end='')
     if trailing_newline: print()
@@ -38,8 +40,8 @@ def check(condition: bool, name: str, error_message: str):
             print_slow('FAILED', prefix='\033[91m', suffix='\033[0m')
             print_slow(error_message)
             print('\033[37m', end='')
-            print_slow('Alternatively, you may pass in the --ignore-failure switch.', delay=0.01)
-            print_slow('However, you will probably encounter a curses error if you do so.', delay=0.01)
+            print_slow('Alternatively, you may pass in the --ignore-failure switch.', delay=0.005)
+            print_slow('However, you will probably encounter a curses error if you do so.', delay=0.005)
             print('\033[0m', end='')
             sys.exit(1)
         else:
@@ -50,7 +52,11 @@ def check(condition: bool, name: str, error_message: str):
 if not config.silent_checks:
     print('\033[?25l', end='')  # disable cursor
     print_slow('Welcome to terminal minesweeper by Mia Celeste')
-    print_slow('Commencing system check.')
+    if len(sys.argv)==1:
+        print()
+        print_slow('No option was specified. Using default settings.')
+        print_slow('To see a list of possible options, run this program with --help.')
+    print_slow('\nCommencing system check.')
 
 v = sys.version_info
 check(v.major == 3 and v.minor >= 8,
@@ -89,7 +95,7 @@ if config.show_animation:
     print()
     print_slow('All system checks completed, ready to sweep some mines（＾ω＾）')
     print_slow('You have selected {} difficulty, which has a {}x{} grid with {} mines\n'.format(
-        config.difficulty,config.board_width,config.board_height,config.mine_count
+        config.difficulty, config.board_width, config.board_height, config.mine_count
     ))
     print_slow(r'Printing a window in the hope that it will come into life ◦°˚\(*❛ ‿ ❛)/˚°◦')
     print_slow('Please lend me your power, Python magic!\nBalabala pew (∩^o^)⊃━☆゜.*', trailing_newline=False)
@@ -100,9 +106,10 @@ from ui import main, calc_first_frame, FG, BG
 # SyntaxError or NameError for lower python versions
 
 if config.show_animation:
+    # show a start up animation before going into curses for the scrolling effect
     for i, row in enumerate(calc_first_frame(height, width)):
         print()
-        print_slow(row, delay=max(0.02 / (i + 1), 0.0005), prefix=f'\033[38;5;{FG}m\033[48;5;{BG}m', suffix='\033[0m',
+        print_slow(row, delay=max(0.01 / (i + 1), 0.0005), prefix=f'\033[38;5;{FG}m\033[48;5;{BG}m', suffix='\033[0m',
                    trailing_newline=False)
         # speeding up so the user doesn't get too bored
         print('\033[0m', end='')
@@ -113,19 +120,40 @@ else:
 
 signal.signal(signal.SIGINT, system_sigint_handler)
 exit_message, exit_status = main()
-# if config.show_startup_animation: print()  # to consume the last \r
+
+# recalculate screen sizes in case it changed
+width, height = os.get_terminal_size()
+
 if exit_message:
+    if config.show_animation:
+        print('\033[0;0H', end='', flush=True)
+        print(((width-1)*' '+'\n')*height,end='')
+        # clear the screen but fast
+        print('\033[0;0H', end='', flush=True)
     print('\033[91m' + exit_message + '\033[0m', flush=True)
 elif exit_status == 0 and config.show_animation:
-    width, height = os.get_terminal_size()
-    # recalculate in case it changed
-    print(f'\033[0m',flush=True,end='')
-    frames=calc_first_frame(height,width)
+
+    def sigint_handler(signum,frame):
+        """
+        sigint handler for impatient people. Skip the final animation.
+        """
+        print('\033[0;0H', end='', flush=True)
+        sys.exit(exit_status)
+
+    signal.signal(signal.SIGINT,sigint_handler)
+
+    # do the reverse animation. using raw ansi sequences
+    # to erase the previously printed static frame
+
+    print(f'\033[0m', flush=True, end='')
+    frames = calc_first_frame(height, width)
     for y in range(height):
         for x in range(width):
-            print(f'\r\033[38;5;{FG}m\033[48;5;{BG}m'+frames[height-y-1][:width-x-1]+'\033[0m'+' '*(x+1),end='',flush=True)
-            time.sleep(max(0.02 / (y + 1), 0.0005))
-        print('\033[F',end='',flush=True)
-    print('\033[0;0H', end='', flush=True)
+            print(f'\r\033[38;5;{FG}m\033[48;5;{BG}m' + frames[height - y - 1][:width - x - 1] + '\033[0m' + ' ' * (
+                        x + 1), end='', flush=True)
+            time.sleep(max(0.01 / (height-y), 0.0005))
+        print('\033[F', end='', flush=True)
     # erase screen and move cursor to top left corner
+    print('\033[0;0H', end='', flush=True)
+    print_slow('Thanks, Python magic. I knew you wouldn\'t fail me ⊂(´・ω・｀⊂)')
 sys.exit(exit_status)
--- ui.py
+++ ui.py
@@ -1,11 +1,12 @@
 import curses.panel
+import random
+import signal
 import time
+import traceback
+from math import floor, ceil
+
 from board import Board
 from config import config
-import traceback
-import random
-from math import floor, ceil
-import signal
 
 FG = 232
 BG = 231
@@ -20,11 +21,11 @@ SYSTEM_DEFAULT = 127
 class FPSMonitor:
     def __init__(self):
         self.data = [time.time() + i / 50 for i in range(100)]
-    
+
     def tick(self):
         self.data.append(time.time())
         self.data.pop(0)
-    
+
     @property
     def fps(self):
         return 1 / ((self.data[-1] - self.data[1]) / 100)
@@ -38,7 +39,7 @@ class Grid:
     A helper class to render the grid with Unicode half characters
     so that the output is approximately square
     """
-    
+
     VBAR = "┃"
     HBAR = "━"
     TOPLEFT = "┏"
@@ -51,7 +52,7 @@ class Grid:
     BOTTOM = "┻"
     CENTER = "╋"
     SPACE = "　"
-    
+
     def __init__(self, width, height):
         """
         the width and height does not include borders
@@ -66,13 +67,13 @@ class Grid:
             for x in range(self.width):
                 row.append(random.choice([self.SPACE]))
             self.grid.append(row)
-    
+
     def put(self, row: int, col: int, char: str):
         if 0x30 <= (charcode := ord(char)) <= 0x40:
             charcode += 0xff10 - 0x30
             char = chr(charcode)
         self.grid[row][col] = char
-    
+
     def render(self):
         res = []
         row = self.TOPLEFT
@@ -85,47 +86,85 @@ class Grid:
             for x in range(self.width):
                 row += ' ' + self.grid[y][x] + ' ' + self.VBAR
             res.append(row)
-            
+
             row = self.LEFT
             for x in range(self.width - 1):
                 row += self.HBAR * 4 + self.CENTER
             row += self.HBAR * 4 + self.RIGHT
             res.append(row)
-        
+
         row = self.VBAR
         for x in range(self.width):
             row += ' ' + self.grid[-1][x] + ' ' + self.VBAR
         res.append(row)
-        
+
         row = self.BOTTOMLEFT
         for x in range(self.width - 1):
             row += self.HBAR * 4 + self.BOTTOM
         row += self.HBAR * 4 + self.BOTTOMRIGHT
         res.append(row)
-        
+
         return res
 
 
 class UIManager:
     def __init__(self, win: curses.window):
-        self.mwin = win
+        self.win = win
         self.board = Board()
         self.mouse_y = 0
         self.mouse_x = 0
         self.monitor = FPSMonitor()
         self.grid = Grid(config.board_width, config.board_height)
         self.should_exit = False
-    
+        self.animation_frame = 0
+
     def exit(self):
         self.should_exit = True
-    
+
     def start(self):
         self.start_time = time.time()
-    
+
+    def paint_window(self):
+        """
+        draw the initial window. always identical to the one
+        produced by calc_first_frame()
+        """
+
+        winh, winw = self.win.getmaxyx()
+        self.win.addstr(0, 1, '╭' + '─' * (winw - 4) + '╮')
+        self.win.addch(1, 1, '│')
+        self.win.addch(1, winw - 2, '│')
+        self.win.addstr(1, (winw - 24) // 2 + 2, 'TERMINAL MINESWEEPER')
+        self.win.addstr(2, 1, '├' + '─' * (winw - 4) + '┤')
+        for y in range(3, winh - 1):
+            self.win.addch(y, 1, '│')
+            self.win.addch(y, winw - 2, '│')
+        self.win.addstr(winh - 1, 1, '╰' + '─' * (winw - 4) + '╯')
+
+    def addstr(self, y: int, x: int, text: str, *args, **kwargs):
+        """
+        A patched version of curses.window.addstr to support animations
+        :param y: the y coordinate
+        :param x: the x coordinate
+        :param text: the text to add
+        :param args: extra arguments passing to curses.window.addstr
+        :param kwargs: extra keywords passing to curses.window.addstr
+        :return: None
+        """
+        if config.show_animation:
+            winh, winw = self.win.getmaxyx()
+            text_to_display = ''
+            # TODO: change the animation to left to right
+            if y <= self.animation_frame:
+                self.win.addstr(y, x, text, *args, **kwargs)
+
+        else:
+            self.win.addstr(y, x, text, *args, **kwargs)
+
     def tick(self):
-        ch = self.mwin.getch()
-        self.mwin.erase()
-        winh, winw = self.mwin.getmaxyx()
+        ch = self.win.getch()
+        self.win.erase()
+        winh, winw = self.win.getmaxyx()
         mouse_button = -1
         if ch == curses.KEY_MOUSE:
             try:
@@ -133,41 +172,32 @@ class UIManager:
             except curses.error:
                 pass
         cell_width = config.use_emojis + 1
-        
-        self.mwin.addstr(4, winw - 15, str((self.mouse_y, self.mouse_x, mouse_button)))
-        self.mwin.addstr(5, winw - 15, str((curses.BUTTON1_CLICKED, curses.BUTTON2_PRESSED)))
-        
-        self.mwin.addch(self.mouse_y, self.mouse_x, curses.ACS_DIAMOND)
-        
-        # Draw the window frame
-        self.mwin.addstr(0, 1, '╭' + '─' * (winw - 4) + '╮')
-        self.mwin.addch(1, 1, '│')
-        self.mwin.addch(1, winw - 2, '│')
-        self.mwin.addstr(1, (winw - 24) // 2 + 2, 'TERMINAL MINESWEEPER')
-        self.mwin.addstr(2, 1, '├' + '─' * (winw - 4) + '┤')
-        for y in range(3, winh - 1):
-            self.mwin.addch(y, 1, '│')
-            self.mwin.addch(y, winw - 2, '│')
-        self.mwin.addstr(winh - 1, 1, '╰' + '─' * (winw - 4) + '╯')
-        
+
+        self.win.addstr(4, winw - 15, str((self.mouse_y, self.mouse_x, mouse_button)))
+        self.win.addstr(5, winw - 15, str((curses.BUTTON1_CLICKED, curses.BUTTON2_PRESSED)))
+
+        self.win.addch(self.mouse_y, self.mouse_x, curses.ACS_DIAMOND)
+
+        self.paint_window()
+
         # overwrite the close button onto the window
-        self.mwin.addstr(1, 3, 'Ｘ')
-        self.mwin.addstr(0, 6, '┬')
-        self.mwin.addstr(1, 6, '│')
-        self.mwin.addstr(2, 6, '┴')
-        
+        self.addstr(1, 3, 'Ｘ')
+        self.addstr(0, 6, '┬')
+        self.addstr(1, 6, '│')
+        self.addstr(2, 6, '┴')
+
         # close button highlight
         if self.mouse_y == 1 and 2 <= self.mouse_x <= 5:
             if mouse_button == curses.BUTTON1_PRESSED:
                 self.exit()
             else:
-                self.mwin.addstr(1, 2, ' Ｘ ', curses.color_pair(2))
-        
-        self.mwin.addstr(4, 3, f'FPS: {round(self.monitor.fps, 2):0<5}')
+                self.addstr(1, 2, ' Ｘ ', curses.color_pair(2))
+
+        self.addstr(4, 3, f'FPS: {round(self.monitor.fps, 2):0<5}')
         for i, row in enumerate(self.grid.render()):
-            self.mwin.addstr(i + 6, 5, row)
-        
-        self.mwin.refresh()
+            self.addstr(i + 6, 5, row)
+
+        self.win.refresh()
         self.monitor.tick()
 
 
@@ -175,14 +205,25 @@ def mainloop(win: curses.window):
     win.clear()
     frame_delay = round(1000 / config.framerate)
     manager = UIManager(win)
-    
+
     signint_handler = lambda signum, frame: manager.exit()
     signal.signal(signal.SIGINT, signint_handler)
     # so it only shuts down after a full frame is rendered
-    
     while True:
+        winh, winw = win.getmaxyx()
+        in_animation= manager.animation_frame < winh
+        manager.animation_frame += in_animation
         manager.tick()
-        if manager.should_exit: return
+        if config.show_animation and in_animation:
+            curses.napms(20)
+        if manager.should_exit:
+            if config.show_animation:
+                while manager.animation_frame>0:
+                    # manager.animation_direction='reversed'
+                    manager.animation_frame-=1
+                    manager.tick()
+                    curses.napms(20)
+            return
         # curses.flushinp()
         # last_frame_time = round((manager.monitor.data[-1] - manager.monitor.data[-2]) * 1000)
         # curses.napms(frame_delay - (last_frame_time - frame_delay))
@@ -195,12 +236,12 @@ def calc_first_frame(height, width):
     :param width: width of the screen
     :return: a list of strings, each representing a line
     """
-    
+
     def pad(line, center=False):
         """a simple """
         if not center: return ' │' + line + ' ' * (width - len(line) - 4) + '│ '
         return ' │' + ' ' * floor((width - len(line) - 4) / 2) + line + ' ' * ceil((width - len(line) - 4) / 2) + '│ '
-    
+
     frame = []
     line = ' ╭' + '─' * (width - 4) + '╮ '
     frame.append(line)
@@ -219,16 +260,16 @@ def main():
         stdscr = curses.initscr()
     except curses.error:
         return 'Cannot initialize curses', 1
-    
+
     exit_message = None
     exit_status = 0
-    
+
     try:
         curses.noecho()
         curses.cbreak()
         stdscr.keypad(True)
         stdscr.nodelay(True)
-        
+
         curses.mousemask(curses.REPORT_MOUSE_POSITION |
                          curses.ALL_MOUSE_EVENTS
                          )
@@ -237,14 +278,14 @@ def main():
         print('\033[?1003h', flush=True)
         # enable mouse tracking with the XTERM API
         # https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h2-Mouse-Tracking
-        
+
         curses.start_color()
         curses.use_default_colors()
         curses.init_pair(DEFAULT, FG, BG)
         curses.init_pair(UI_HIGHLIGHT, UI_HIGHLIGHT_FG, UI_HIGHLIGHT_BG)
         curses.init_pair(SYSTEM_DEFAULT, -1, -1)
         stdscr.bkgd(' ', curses.color_pair(1))
-        
+
         # initialization complete
         mainloop(stdscr)
     except UIError as err:
