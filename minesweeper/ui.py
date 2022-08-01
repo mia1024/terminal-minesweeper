a59d6c895d3d8f4a2e5a003996649484b47cf0ed
Mia Celeste <mia@miaceleste.dev>
2022-07-31 23:48:47 -0700

    Stopped rendering ⓘ in no-emoji mode

--- minesweeper/ui.py
+++ minesweeper/ui.py
@@ -383,35 +383,35 @@ class GridWidget(Widget):
 
                 # special case for first column
                 if not x:
-                    self.addstr(y * 2 + 1, 0, box(up=tl,down=tl))
-                    self.addstr(y * 2 + 3, 0, box(up=bl,down=bl))
-                    self.addstr(y * 2 + 2, 0, box(up=tl,down=bl,right=tl or bl))
+                    self.addstr(y * 2 + 1, 0, box(up = tl, down = tl))
+                    self.addstr(y * 2 + 3, 0, box(up = bl, down = bl))
+                    self.addstr(y * 2 + 2, 0, box(up = tl, down = bl, right = tl or bl))
 
                 # special case for last column
                 if x == self.board.width - 2:
-                    self.addstr(y * 2 + 1, x * 5 + 10, box(up=tr,down=tr))
-                    self.addstr(y * 2 + 3, x * 5 + 10, box(up=br,down=br))
-                    self.addstr(y * 2 + 2, x * 5 + 10, box(up=tr,down=br,left=tr or br))
+                    self.addstr(y * 2 + 1, x * 5 + 10, box(up = tr, down = tr))
+                    self.addstr(y * 2 + 3, x * 5 + 10, box(up = br, down = br))
+                    self.addstr(y * 2 + 2, x * 5 + 10, box(up = tr, down = br, left = tr or br))
 
                 # special case for first row
                 if not y:
-                    self.addstr(y * 2, x * 5 + 1, box(left=tl,right=tl) * 4)
-                    self.addstr(y * 2, x * 5 + 6, box(left=tr,right=tr) * 4)
-                    self.addstr(y * 2, x * 5 + 5, box(left=tl,right=tr,down=tl or tr))
+                    self.addstr(y * 2, x * 5 + 1, box(left = tl, right = tl) * 4)
+                    self.addstr(y * 2, x * 5 + 6, box(left = tr, right = tr) * 4)
+                    self.addstr(y * 2, x * 5 + 5, box(left = tl, right = tr, down = tl or tr))
 
                 # special case for last row
                 if y == self.board.height - 2:
-                    self.addstr(y * 2 + 4, x * 5 + 1, box(left=bl,right=bl) * 4)
-                    self.addstr(y * 2 + 4, x * 5 + 6, box(left=br,right=br) * 4)
-                    self.addstr(y * 2 + 4, x * 5 + 5, box(left=bl,right=br,up=bl or br))
+                    self.addstr(y * 2 + 4, x * 5 + 1, box(left = bl, right = bl) * 4)
+                    self.addstr(y * 2 + 4, x * 5 + 6, box(left = br, right = br) * 4)
+                    self.addstr(y * 2 + 4, x * 5 + 5, box(left = bl, right = br, up = bl or br))
 
                 # horizontal lines
-                self.addstr(y * 2 + 2, x * 5 + 1, box(left=tl or bl,right=tl or bl) * 4)
-                self.addstr(y * 2 + 2, x * 5 + 6, box(left=tr or br,right=tr or br) * 4)
+                self.addstr(y * 2 + 2, x * 5 + 1, box(left = tl or bl, right = tl or bl) * 4)
+                self.addstr(y * 2 + 2, x * 5 + 6, box(left = tr or br, right = tr or br) * 4)
 
                 # vertical lines
-                self.addstr(y * 2 + 1, x * 5 + 5, box(up=tl or tr,down=tl or tr))
-                self.addstr(y * 2 + 3, x * 5 + 5, box(up=bl or br,down=bl or br))
+                self.addstr(y * 2 + 1, x * 5 + 5, box(up = tl or tr, down = tl or tr))
+                self.addstr(y * 2 + 3, x * 5 + 5, box(up = bl or br, down = bl or br))
 
                 # the center of the cluster
                 self.addstr(y * 2 + 2, x * 5 + 5, box(tl or tr, bl or br, tl or bl, tr or br))
@@ -422,10 +422,10 @@ class GridWidget(Widget):
         br = not self.board[-1][-1].is_revealed  # bottom right
 
         # add the corners of the board
-        self.addstr(0, 0, box(right=tl,down=tl))
-        self.addstr(0, width - 1, box(left=tr,down=tr))
-        self.addstr(height - 1, 0, box(up=bl,right=bl))
-        self.addstr(height - 1, width - 1, box(up=br,left=br))
+        self.addstr(0, 0, box(right = tl, down = tl))
+        self.addstr(0, width - 1, box(left = tr, down = tr))
+        self.addstr(height - 1, 0, box(up = bl, right = bl))
+        self.addstr(height - 1, width - 1, box(up = br, left = br))
 
         if self.root.keyboard_mode:
             self.subwidgets[self.selected_cell].highlight(True)
@@ -543,8 +543,11 @@ class HelpWidget(Widget):
     @property
     def w(self):
         if not self.is_active:
-            # +1 for the full width character ⓘ
-            return len("ⓘ press i for help") + 1
+            if not config.use_emojis:
+                return len("press i for help")
+            else:
+                # +1 for the full width character ⓘ
+                return len("ⓘ press i for help") + 1
         else:
             return 54
 
@@ -563,7 +566,11 @@ class HelpWidget(Widget):
 
     def render(self):
         if not self.is_active:
-            self.addstr(0, 0, "ⓘ press i for help")
+            if not config.use_emojis:
+                # some terminals, e.g. terminus, can't render ⓘ
+                self.addstr(0, 0, "ⓘ press i for help")
+            else:
+                self.addstr(0, 0, "press i for help")
         else:
             mouse_y = self.mouse_y()
             mouse_x = self.mouse_x()
@@ -596,6 +603,7 @@ class HelpWidget(Widget):
             self.parent.force_rerender = True
 
     def mouse_event(self, y, x, mouse):
+        if not config.use_emojis: return
         if MouseEvent.BUTTON1_RELEASED in mouse:
             if not self.is_active:
                 if y == 0 and x == 0:
@@ -847,10 +855,10 @@ class RootWidget(Widget):
         # caps the framerate by postponing rendering
         if config.framerate:
             self.render()
-            curses.napms(floor((1/config.framerate-(time.time()-self.monitor.last_frame))*1000))
+            curses.napms(floor((1 / config.framerate - (time.time() - self.monitor.last_frame)) * 1000))
         else:
             self.render()
-            curses.napms(1) # voluntary context switch
+            curses.napms(1)  # voluntary context switch
 
     def render(self):
         """renders the entire window{"""
