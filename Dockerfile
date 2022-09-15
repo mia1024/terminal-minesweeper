6316c5ff7ef4a22246c297255c2bb8f29341c6ae
Mia Celeste <mia@miaceleste.dev>
2022-09-15 00:46:37 -0700

    Basic completer

--- Dockerfile
+++ Dockerfile
@@ -4,17 +4,24 @@ WORKDIR /minesweeper
 COPY . /minesweeper
 RUN pip install . --compile
 WORKDIR /
-RUN rm -rf /minesweeper
 
 COPY mineshell.py /
+
+RUN rm -rf /media /opt /srv /var
+RUN rm -rf /usr/local/lib/python3.10/site-packages/p*  # pip
+RUN rm -rf /usr/local/lib/python3.10/site-packages/s*  # setuptools
+RUN rm -rf /usr/local/lib/python3.10/site-packages/t*  # terminal-minesweeper
+RUN rm -rf /usr/local/lib/python3.10/site-packages/w*  # wheel
+RUN rm -rf /minesweeper /usr/bin /usr/sbin /usr/share /bin/* /home /sbin /etc/ssl /etc/apk /root /etc/passwd /etc/shadow /etc/network /tmp
 
 # Remove layers
 FROM scratch
 COPY --from=build / /
 
-USER 1000
+USER 65534
 
 ENV PS1="$ "
+ENV PATH="/usr/local/bin"
 ENV TERM="xterm-256color"
 
 ENTRYPOINT ["/usr/local/bin/python", "mineshell.py"]
--- mineshell.py
+++ mineshell.py
@@ -1,12 +1,12 @@
 #! /usr/bin/python
 
-import os,sys
+import os, sys
 import readline
 import shlex
 import signal
 from minesweeper import __version__, config, main
 
-fps=os.environ.get("MINESWEEPER_FORCE_FPS")
+fps = os.environ.get("MINESWEEPER_FORCE_FPS")
 print("\033[95mWelcome! Run \033[96mminesweeper\033[95m to start the game. "
       "Run \033[96mminesweeper --help\033[95m for a list of available options. Have fun ^_^\033[0m\n")
 
@@ -16,21 +16,41 @@ if fps is not None:
         print(f"\033[38;5;244mTo reduce server stress, your frame rate is limited to {fps} fps. "
               f"You can download the game at https://minesweeper.mia1024.io/ to enjoy uncapped performance. "
               f"If you like the game, please consider giving this project a star on GitHub.\033[0m")
-    except (ValueError,TypeError):
+    except (ValueError, TypeError):
         pass
 sys.stdout.flush()
+
 
 def exit_handle(signum, frame):
     print()
     sys.exit(0)
 
-signal.signal(signal.SIGINT,exit_handle)
-signal.signal(signal.SIGHUP,exit_handle)
+
+signal.signal(signal.SIGINT, exit_handle)
+signal.signal(signal.SIGHUP, exit_handle)
 signal.signal(signal.SIGTERM, exit_handle)
+
+
+def completer(i, s):
+    if readline.get_line_buffer().startswith("minesweeper "):
+        return None
+    if not i:
+        if s == 0:
+            return "minesweeper "
+        if s == 1:
+            return "exit"
+    elif s == 0:
+        return "minesweeper "
+    return None
+
+
+readline.set_completer(completer)
+readline.parse_and_bind("tab: complete")
+readline.set_completer_delims(" ")
 
 while True:
     try:
-        args=shlex.split(input("$ "))
+        args = shlex.split(input("$ "))
     except EOFError:
         print()
         sys.exit(0)
